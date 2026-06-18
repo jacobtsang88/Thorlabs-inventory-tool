885e29aa921da5155edb4382a63ca6c5f9468d52
jacobtsang88 <jacobtsang88@gmail.com>
2026-06-18 11:17:15 -0400

    Update fart.py

--- fart.py
+++ fart.py
@@ -16,15 +16,17 @@ def get_raw_data(
         browser = p.chromium.launch(headless=True) #launch chromium browser, no GUI
         page = browser.new_page() #open new page
         page.goto(product_url, timeout=30000) #go to the product page, wait up to 30s
+        page.wait_for_load_state("networkidle", timeout=20000)
+
 
         #find family link
         family_link = page.locator("a", has_text=re.compile(r"product family", re.IGNORECASE)).first
         if not family_link.is_visible():
             print("[ERROR] Could not find a 'Product Family' link on the product page.")
             browser.close()
-            return None
+            return None  
         return family_link.get_attribute("href")
     browser.close()
 
 
-get_raw_data("ibd14p8")
+print(get_raw_data("ibd14p8"))
